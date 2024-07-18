c1d19b65fede9f5d290a5b9155e4fb666d7c903c
Nikhil <nsjniksam@gmail.com>
2024-07-18 20:01:47 +0530

    app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React from 'react';
+import Navigator from './components/Navigator';
+import './styles.css';
+import backgroundImage from './assets/sathyabhama-university.jpg'; // Import the image
 
 function App() {
+  const appStyle = {
+    background: `url(${backgroundImage}) no-repeat center center fixed`,
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={appStyle}>
+      <Navigator />
     </div>
   );
 }
